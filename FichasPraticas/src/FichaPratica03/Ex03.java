a25c5c00ce5cc49c4487263efdb963009458007d
Vitor Santos <vmvs007@gmail.com>
2025-10-20 14:26:32 +0100

    FichaPratica03

---
+++ FichasPraticas/src/FichaPratica03/Ex03.java
@@ -0,0 +1,14 @@
+package FichaPratica03;
+
+public class Ex03 {
+    public static void main(String[] args) {
+
+        int num = 531;
+
+        while (num <= 750) {
+            System.out.println(num);
+            num = num + 2;
+        }
+
+    }
+}
