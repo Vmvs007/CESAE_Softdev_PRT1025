dce632dc907bbe6864ffebb2ce047bd63012a44e
Vitor Santos <vmvs007@gmail.com>
2025-10-28 14:46:01 +0000

    FichaPratica05

---
+++ FichasPraticas/src/FichaPratica05/Ex02.java
@@ -0,0 +1,28 @@
+package FichaPratica05;
+
+import java.util.Scanner;
+
+public class Ex02 {
+    public static void main(String[] args) {
+
+        // Importar o Scanner
+        Scanner input = new Scanner(System.in);
+
+        // Declarar variáveis
+        double[] comissoes = new double[12];
+        double totalAnual = 0;
+
+        // Preencher o vetor
+        for (int i = 0; i < 12; i++) {
+            System.out.print("Insira comissões no vetor[" + i + "]: ");
+            comissoes[i] = input.nextInt();
+        }
+
+        // Calcular o total
+        for (int i = 0; i < 12; i++) {
+            totalAnual += comissoes[i];
+        }
+
+        System.out.println("Total Comissões Anuais: " + totalAnual + " €");
+    }
+}
---
+++ FichasPraticas/src/FichaPratica05/Ex04.java
@@ -0,0 +1,35 @@
+package FichaPratica05;
+
+import java.util.Scanner;
+
+public class Ex04 {
+    public static void main(String[] args) {
+
+        // Importar o Scanner
+        Scanner input = new Scanner(System.in);
+
+        // Declarar variáveis
+        int[] vetor = new int[10];
+
+
+        // Leitura de números para o vetor
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.print("Insira no vetor[" + i + "]: ");
+            vetor[i] = input.nextInt();
+        }
+
+        int menor = vetor[0];
+
+        // Encontrar o maior elemento
+        for (int i = 0; i < vetor.length; i++) {
+
+            if (vetor[i] < menor) {
+                menor = vetor[i];
+            }
+        }
+
+        System.out.println("Menor elemento: " + menor);
+
+
+    }
+}
